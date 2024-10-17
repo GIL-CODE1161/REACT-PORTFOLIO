ef450d9f7702c0fa51f136078f57c59711ee3a81
George Ocheme Onehlofiochi <onehlofiochiocheme@gmail.com>
2024-10-17 14:57:56 +0100

    I added more projects

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,6 +2,9 @@ import project1 from "../assets/projects/project-1.jpg";
 import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
+import project5 from "../assets/projects/project-5.jpg";
+import project6 from "../assets/projects/project-6.jpg";
+import project7 from "../assets/projects/project-7.jpg";
 import laptop from "../assets/projects/laptop.jpg";
 
 export const HERO_CONTENT = `Expert Front-end Developer with four years of expertise in front-end web application design, development, and maintenance. A 20% increase in page load times was measured and resulted in a rise in website conversion rates. Initiated and oversaw the rollout of a new front-end development system, resulting in a 20% boost in website performance and a 15% decrease in development time. adept at working in cross-functional teams and meeting deadlines and budgets for projects.`;
@@ -9,69 +12,75 @@ export const HERO_CONTENT = `Expert Front-end Developer with four years of exper
 export const ABOUT_TEXT = `I am a dedicated and versatile front end developer with a passion for creating efficient, dynamic and user-friendly interfaces for web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React.js, Next.js, Node.js, Vue.js, Angular.js and Express.js. My journey in web development began with a deep curiosity for how things work and appear visually , and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
+  
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
-  },
-  {
-    year: "2022 - 2023",
+    year: "2022 - present",
     role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+    company: "Tudevs",
+    description: `Designed and developed user interfaces for web applications using Next.js, Vue.js, Angular.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["HTML", "CSS", "Vue.js", "React.JS", "Angular.JS", "Next.JS", "MongoDB", "Tailwind CSS"],
   },
 ];
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "E-Commerce Website - UI",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "An e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "Tailwind CSS", "React"],
   },
   {
-    title: "Task Management App",
+    title: "Metaversus-App UI",
     image: project2,
     description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+      "The metaverse project I developed is a cutting-edge virtual ecosystem designed to seamlessly blend the physical and digital worlds, offering users an immersive, interactive experience like never before. The platform serves as a hub for social, professional, and entertainment activities, enabling users to explore, create, and connect in entirely new ways.",
+    technologies: ["HTML", "Tailwind CSS", "Next JS"],
+  },
+  {
+    title: "VUE Weather-App",
+    image: project3,
+    description:
+      "I developed a modern weather app using Vue.js, designed to provide users with accurate, real-time weather data in a clean and intuitive interface. The app is optimized for speed, user experience, and accessibility, ensuring users can quickly check the weather, wherever they are.",
+    technologies: ["HTML", "Tailwind CSS", "VUE JS"],
+  },
+  {
+    title: "Apple-website UI",
+    image: project4,
+    description: "The user interface I developed for the Apple-inspired website embodies clean, minimalist aesthetics with a focus on intuitive usability, reflecting Apple's design philosophy.The layout is sleek and uncluttered,with ample white space to allow each element to stand out while ensuring the content remains the focal point.",
+    technologies: ["HTML", "Tailwind CSS", "React", "three js", "Framer motion"],
+  },
+  {
+    title: "VirtualR Website UI",
+    image: project5,
+    description: "The virtual video game app I developed is an interactive, highly immersive platform that transports players into vibrant, 3D worlds full of adventure, challenges, and social interactions. Designed to cater to both casual and competitive gamers, the app offers an engaging gameplay experience that combines cutting-edge technology with a seamless user interface.he user interface I developed for the Apple-inspired website embodies clean, minimalist aesthetics with a focus on intuitive usability, reflecting Apple's design philosophy.The layout is sleek and uncluttered,with ample white space to allow each element to stand out while ensuring the content remains the focal point.",
+    technologies: ["HTML", "Tailwind CSS", "React", "Framer motion"],
   },
   {
     title: "PORTFOLIO WEBSITE",
-    image: project3,
+    image: project6,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Tailwind", "Framer motion"],
+    technologies: ["HTML", "Tailwind CSS", "React", "Framer motion"],
+  },
+  {
+    title: "COUNTER-APP",
+    image: project7,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "Angular JS"],
   },
   {
     title: "DATA FINANCE ANALYTICS APP",
     image: laptop,
     description:
       " A platform that helps us see insights and meaningful data that we might not otherwise detect.The platform is designed to help Business analytics focuses on using insights derived from data to make more informed decisions that will help organizations increase sales, reduce costs, and make other business improvements.",
-    technologies: ["HTML", "CSS", "React js", "React typed Package"],
+    technologies: ["HTML", "CSS", "React js", "React typed"],
   },
 ];
 
 export const CONTACT = {
-  address: "23 Fifth Avenue, Gwarimpa, Abuja. ",
+  address: "23 Fifth Avenue,Gwarimpa, Abuja. ",
   phoneNo: "+234 8060613523 ",
   email: "cis2ene.dev@gmail.com",
 };
